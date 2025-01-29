f561a5429628c5cb4039fc2773dfd01ea264af24
Cesar ubuntu <cesarwamartinez@gmail.com>
2025-01-28 19:57:53 -0500

    Se añade el carrusel

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import MangaList from "../Components/MangaList";
 import { GET_MANGA_LIST } from "../Request/RequestMangaList";
 import { useQuery } from "@apollo/client";
